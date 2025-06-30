140b5ce07356309ff606e713aaaad73ee0828bb2
TheDataDawg <jakerhotchkiss@gmail.com>
2025-06-29 23:34:17 -0600

    Build working Airflow DAG to load 2023 quarterly CSVs from S3 to Redshift using COPY

--- docker/dags/redshift_etl_dag.py
+++ docker/dags/redshift_etl_dag.py
@@ -3,56 +3,61 @@ from airflow.operators.python import PythonOperator
 from airflow.operators.empty import EmptyOperator
 from airflow.hooks.base import BaseHook
 from datetime import datetime
-from extract_s3 import download_from_s3
-from load_redshift import load_to_redshift
+import psycopg2
 
 def get_redshift_credentials(conn_id='redshift_capstone'):
     conn = BaseHook.get_connection(conn_id)
     return {
-        'redshift_host': conn.host,
-        'redshift_port': conn.port,
-        'redshift_user': conn.login,
-        'redshift_password': conn.password,
-        'redshift_db': conn.schema
+        'host': conn.host,
+        'port': conn.port,
+        'user': conn.login,
+        'password': conn.password,
+        'dbname': conn.schema
     }
 
+def copy_to_redshift(**kwargs):
+    creds = get_redshift_credentials()
+    iam_role = 'arn:aws:iam::900546257653:role/RedshiftS3AccessRole'
+    s3_paths = [
+        's3://flight-cost-capstone-jhotchkiss/raw/year=2023/quarter=Q1/2023_Q1_final.csv',
+        's3://flight-cost-capstone-jhotchkiss/raw/year=2023/quarter=Q2/2023_Q2_final.csv',
+        's3://flight-cost-capstone-jhotchkiss/raw/year=2023/quarter=Q3/2023_Q3_final.csv',
+        's3://flight-cost-capstone-jhotchkiss/raw/year=2023/quarter=Q4/2023_Q4_final.csv',
+    ]
+    
+    conn = psycopg2.connect(**creds)
+    cur = conn.cursor()
+
+    for path in s3_paths:
+        copy_sql = f"""
+        COPY flight_fares (year, quarter, origin, dest, reporting_carrier, passengers, market_fare, market_distance)
+        FROM '{path}'
+        IAM_ROLE '{iam_role}'
+        CSV
+        IGNOREHEADER 1;
+        """
+        cur.execute(copy_sql)
+        conn.commit()
+        print(f"Loaded: {path}")
+
+    cur.close()
+    conn.close()
+
 with DAG(
-    dag_id='redshift_etl_dag',
+    dag_id='redshift_copy_from_s3_dag',
     start_date=datetime(2024, 1, 1),
     schedule_interval=None,
     catchup=False,
-    tags=['capstone', 'etl']
+    tags=['capstone', 'redshift', 's3']
 ) as dag:
 
     start = EmptyOperator(task_id='start')
 
-    extract = PythonOperator(
-        task_id='extract_from_s3',
-        python_callable=download_from_s3,
-        op_kwargs={
-            'bucket_name': 'flight-cost-capstone-jhotchkiss',
-            'object_key': 'raw/2023_Q1_clean.csv',
-            'download_path': '/opt/airflow/data/2023_Q1_clean.csv'
-        }
-    )
-
-    def load_with_conn(**kwargs):
-        creds = get_redshift_credentials()
-        load_to_redshift(
-            redshift_host=creds['redshift_host'],
-            redshift_port=creds['redshift_port'],
-            redshift_user=creds['redshift_user'],
-            redshift_password=creds['redshift_password'],
-            redshift_db=creds['redshift_db'],
-            table_name='flight_fares',
-            csv_path='/opt/airflow/data/2023_Q1_clean.csv'
-        )
-
     load = PythonOperator(
-        task_id='load_to_redshift',
-        python_callable=load_with_conn
+        task_id='copy_csvs_to_redshift',
+        python_callable=copy_to_redshift
     )
 
     end = EmptyOperator(task_id='end')
 
-    start >> extract >> load >> end
+    start >> load >> end
